9857fce171813f3086d5de9aa018a6a0271ed668
Helio Silva <hos3@academy.cin.ufpe.br>
2020-11-12 11:55:20 -0300

    add projeto no xcode

---
+++ Autime/Autime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Autime
+//
+//  Created by Hélio Silva on 12/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
