fe0e7b2c9124b51e520a58f3bd7c8be6a8ec802c
Dudu Ramos <ramos.luiseduardo@hotmail.com>
2020-11-23 11:36:44 -0300

    ActionShit ajustado

---
+++ Autime/Autime/MenuPaisViewController.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Autime
+//
+//  Created by Luis Eduardo Ramos on 23/11/20.
+//
+
+import Foundation
---
+++ Autime/Autime/PastWeeksPaisViewController.swift
@@ -0,0 +1,8 @@
+//
+//  PastWeeksPaisViewController.swift
+//  Autime
+//
+//  Created by Luis Eduardo Ramos on 23/11/20.
+//
+
+import Foundation
