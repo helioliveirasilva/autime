8ad60b20aec74e13ad41e2d407fcb289ebad9db1
Dudu Ramos <ramos.luiseduardo@hotmail.com>
2020-11-23 11:37:28 -0300

    ActionShit ajustado 1.2

--- Autime/Autime/ThisWeeksPaisViewController.swift
+++ Autime/Autime/ThisWeeksPaisViewController.swift
@@ -5,4 +5,13 @@
 //  Created by Luis Eduardo Ramos on 23/11/20.
 //
 
-import Foundation
+import UIKit
+
+class ThisWeeksPaisViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+    }
+}
