124e9742b5f750cc079729871985b77e03326fc1
Victor <avieira.victor@gmail.com>
2020-12-09 23:32:43 -0300

    CollectionView AddSubAtividade

---
+++ Autime/Autime/Controllers/AddSubAtividadeView.swift
@@ -0,0 +1,20 @@
+//
+//  AddSubAtividadeView.swift
+//  Autime
+//
+//  Created by Victor Vieira on 09/12/20.
+//
+
+import UIKit
+
+class AddSubAtividadeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Autime/Autime/Models/SubAtividadeCell.swift
@@ -0,0 +1,12 @@
+//
+//  SubAtividadeCell.swift
+//  Autime
+//
+//  Created by Victor Vieira on 09/12/20.
+//
+
+import UIKit
+
+class SubAtividadeCell: UICollectionViewCell {
+    
+}
